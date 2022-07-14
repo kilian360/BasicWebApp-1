5a55f5bbce527930804c65804793049ad2e40bfd
kilian <kilian.haas@in.tum.de>
2022-07-14 14:55:11 +0200

    test

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -10,7 +10,7 @@ public class QueryProcessor {
         if (query.contains("shakespeare")) {
             return "William Sfhakespeare (26 April 1564 - 23 April 1616) was an " +
                     "Englisdh poet, playwright, and acktor, widely regarded as the greatest " +
-                    "writer in the Engdkljlish language and the world's pre-eminent dramatist.";
+                    "writer ifn the Engdkljlish language and the world's pre-eminent dramatist.";
         } else if (query.contains("name")) {
            return "MyTeam";
         } else { // TODO extend the programm here
